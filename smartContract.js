f74300265be28f6dd6fbf18850a4420f372ea8fb
mohdnuman <mohammadnuman123@gmail.com>
2022-04-24 03:16:59 +0530

    added more contracts

--- smartContract.js
+++ smartContract.js
@@ -13,14 +13,21 @@ const provider = new Web3.providers.HttpProvider(
 );
 web3 = new Web3(provider);
 
-async function getBalance(address, contract) {
+async function getPoolData(address, contract) {
   const Instance = new web3.eth.Contract(Abi, contract);
 
   let LPtokensReceived = await Instance.methods.balanceOf(address).call();
   let LPtotalSupply = await Instance.methods.totalSupply().call();
   let rewards = await Instance.methods.earned(address).call();
   let pool = await Instance.methods.stakingToken().call();
+  if(contract==="0xcF05a60bCBC9c85cb2548DAfDC444c666A8F466a")
+  LPtokensReceived = LPtokensReceived / 1.45;
+  else if(contract==="0xe6e5220291CF78b6D93bd1d08D746ABbC115C64b")
+  LPtokensReceived = LPtokensReceived / 9.6;
+  else
   LPtokensReceived = LPtokensReceived / 1.1;
+
+  
 
   const poolInstance = new web3.eth.Contract(poolAbi, pool);
 
@@ -51,11 +58,10 @@ async function getBalance(address, contract) {
   token1amount = token1amount.toFixed(2);
   if ((token0amount != 0, token1amount != 0)) {
     console.log(Symbol0, "+", Symbol1, token0amount, "+", token1amount);
-    console.log("rewards:", (rewards / 10 ** Decimal1).toFixed(2));
+    console.log("rewards:", (rewards / 10 ** Decimal1).toFixed(2),'GOVI');
   }
 }
-
-async function getData2(userAddress) {
+async function getDataOfETHGOVIrewards(userAddress) {
   const contract = "0x40d203332b0A262F1a371ae9dA1788fe6825A6F6";
 
   const Instance = new web3.eth.Contract(Abi2, contract);
@@ -71,8 +77,7 @@ async function getData2(userAddress) {
     console.log("rewards:", rewards, "GOVI");
   }
 }
-
-async function getData3(userAddress) {
+async function getDataOfETH(userAddress) {
   const contract = "0x5005e8Dc0033E78AF80cfc8d10f5163f2FcF0E79";
 
   const Instance = new web3.eth.Contract(Abi2, contract);
@@ -82,37 +87,62 @@ async function getData3(userAddress) {
   balance = (balance / 10 ** 21 / 1.22).toFixed(2);
 
   if (balance != 0) {
-    console.log("balance:", balance);
+    console.log("balance:", balance, "ETH");
   }
 }
+async function getDataOFUSDTGOVIrewards(userAddress) {
+  const contract = "0xDB14a3B5BdFd0cD7b2Ef5075b2689290d9eDC915";
 
-async function getData4(userAddress) {
+  const Instance = new web3.eth.Contract(Abi2, contract);
+
+  let balance = await Instance.methods.balanceOf(userAddress).call();
+  let rewards = await Instance.methods.earned(userAddress).call();
+
+  balance = ((balance / 10 ** 18) * 1.3).toFixed(2);
+  rewards = (rewards / 10 ** 18).toFixed(2);
+
+  if (balance != 0) {
+    console.log("balance:", balance, "USDT");
+    console.log("rewards:", rewards, "GOVI");
+  }
+}
+async function getDataOfGOVIstaking(userAddress) {
   const contract = "0xDb3130952eD9b5fa7108deDAAA921ae8f59beaCb";
 
   const Instance = new web3.eth.Contract(Abi3, contract);
 
   let tokens = await Instance.methods.getClaimableTokens().call();
+  let stake = await Instance.methods.stakes(userAddress).call();
+  if(stake!=0)
+  console.log("stake:", (stake / 10 ** 18).toFixed(2));
   for (let i = 0; i < tokens.length; i++) {
-    let balance = await Instance.methods.profitOf(userAddress,tokens[i]).call();
+    let balance = await Instance.methods
+      .profitOf(userAddress, tokens[i])
+      .call();
 
-    if (balance != 0) {
-      if(i==0)
-      console.log("balance:", (balance/10**8).toFixed(2),"WETH");
-      else if(i==1)
-      console.log("balance:", (balance/10**17).toFixed(2),'USDC');
-      else if(i==2)
-      console.log("balance:", (balance/10**6).toFixed(2),'USDT');
+    let balance1 = (balance / 10 ** 8).toFixed(2);
+    let balance2 = (balance / 10 ** 17).toFixed(2);
+    let balance3 = (balance / 10 ** 6).toFixed(2);
+
+    if (stake != 0) {
+      if (i == 0 && balance1 != 0) console.log("balance:", balance1, "WETH");
+      else if (i == 1 && balance2 != 0)
+        console.log("balance:", balance2, "USDC");
+      else if (i == 2 && balance3 != 0)
+        console.log("balance:", balance3, "USDT");
     }
   }
 }
 
-let address = "0x4371219225bf02fe7d9a5d5247982ba6b4bf42e9";
+let address = "0x967d1e5c987f1ecdb27179bcb4153b2fd0167c92";
 let pools = [
   "0x936Dd3112a9D39Af39aDdA798503D9E7E7975Fb7",
   "0xe6e5220291CF78b6D93bd1d08D746ABbC115C64b",
+  "0xcF05a60bCBC9c85cb2548DAfDC444c666A8F466a"
 ];
-for (let i = 0; i < 2; i++) getBalance(address, pools[i]);
+for (let i = 0; i < 3; i++) getPoolData(address, pools[i]);
 
-getData2(address);
-getData3(address);
-getData4(address);
+getDataOfETHGOVIrewards(address);
+getDataOfETH(address);
+getDataOFUSDTGOVIrewards(address);
+getDataOfGOVIstaking(address);
